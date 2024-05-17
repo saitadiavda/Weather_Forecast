3fe67bd344974e2a1cebc2500bb31b0b1ef02f13
macsoftechnologies <macsoftechnologies@gmail.com>
2024-05-17 14:20:24 +0530

    Intergrated weather forecast API

---
+++ src/app/Services/weather-forecast.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherForecastService {
+
+  constructor(private http: HttpClient) { }
+  
+  getforeCastData(location: any) {
+    return this.http.get(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=1635890035cbba097fd5c26c8ea672a1`);    
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { WeatherForecastService } from './Services/weather-forecast.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'weatherforecast';
+  title = 'wheather_forecast';
+  weatherSearchForm: FormGroup;
+  weatherforecastData: any;
+  loader: boolean = false;
+  nodataFound: boolean = true;
+  errorMessage: any;
+  cityNotFound: boolean = false;
+  constructor(private apiservice: WeatherForecastService) {
+    this.weatherSearchForm = new FormGroup({
+      location: new FormControl('', [Validators.required])
+    })
+  }
+
+  weatherData() {
+    if (this.weatherSearchForm.valid) {
+      this.loader = true;
+      this.nodataFound = false;
+      let weatherformobj = {
+        location: this.weatherSearchForm.value.location
+      }
+      this.apiservice.getforeCastData(weatherformobj.location).subscribe(
+        {
+          next : (resp: any) => {
+            if (resp.cod == 200) {
+              this.weatherforecastData = resp.list.slice(0, 20);
+              this.loader = false;
+              this.cityNotFound = false;
+            }
+            else {}
+          },
+          error: (error: any) => {
+            this.loader = false;
+            this.errorMessage = error.error.message;
+            this.cityNotFound = true;
+            this.weatherforecastData = [];
+          },
+        }
+      )
+    }
+  }
 }
